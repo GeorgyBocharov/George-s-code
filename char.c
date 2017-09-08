454347449a884d43d3eedcbc922ec4272ae27e0d
George <georgybocharovfrtc@gmail.com>
2017-09-08 11:54:04 +0300

    deadline 15.09

---
+++ char.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void Generatestring (int n, char* str);
+int main()
+{
+ int i, p, k;
+ char* s;
+ scanf("%d", &p);
+ k = (1 << p) - 1;
+ s = (char*)malloc(sizeof(char)*k);
+ Generatestring (p, s);
+ printf ("%s", s);
+}
+void Generatestring (int n, char* str)
+{
+ int m=0 ,k;
+ for (k = 1; k <= n; k++)
+ {
+  strncpy(&str[m+1], str, sizeof(char) * m);
+  str[m] = 'a' + k - 1;
+  m = (1 << k) - 1;
+ }
+}
+ 
