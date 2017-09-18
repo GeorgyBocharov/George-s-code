06a4547cf9ab7347af45ef84da1083c62a8f7566
Vlad <vlad.miryaha@gmail.com>
2017-09-19 01:45:55 +0300

    fixit

--- split.c
+++ split.c
@@ -10,12 +10,19 @@ s = (char*)malloc(sizeof(char));
 tab = (char*)malloc(sizeof(char));
 while ((s[i]=getchar())!='\n')
 {
+  // fixit: отступ = 2 или 4 пробела ... не 1
+  // fixit: пробелы вокруг бинарных операторов: (i + 1) * sizeof(char)
+ 
+  // выделение памяти в куче - дорогая операция, если хотите именно в куче получить память, то можно было бы
+  // объем памяти увеличивать не на 1, в 2 раза при необходимости ... тогда число realloc'ов будет log2(n), а
+  // лишней памяти отъедите максимум в 2 раза больше чем нужно
  s = (char*) realloc(s, (i+1)*sizeof(char));
  i++;
 }
 s[i]='\0';
 while ((tab[j]=getchar())!='\n')
 {
+ // fixit: у вас цикл while похож на предыдущий. копипаста быть не должно. вынесите в отдельную ф-ю.
  tab = (char*) realloc(tab, (j+1)*sizeof(char));
  j++;
 }
@@ -32,6 +39,7 @@ char* point;
   point = strtok(s, tab);
   while (point != NULL)
   {
+   // fix it: мы обговарива сигнатуру ф-и, вывод слов нужно вынести из ф-и
     printf("%s\n", point);
     point = strtok (NULL, tab);
   }
