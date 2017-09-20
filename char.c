b6efb04efbab01286ef799d1645170c3ac5218b5
George <georgybocharovfrtc@gmail.com>
2017-09-20 18:43:15 +0300

    fixed version for 15.09

--- char.c
+++ char.c
@@ -4,25 +4,24 @@
 void Generatestring (int n, char* str);
 int main()
 {
- // обычно отсутуп делают в 2 или 4 пробела ... 1 мало
- int i, p, k;
- char* s;
- scanf("%d", &p);
- k = (1 << p) - 1;
- s = (char*)malloc(sizeof(char)*k);
- // пробелы вокруг *, + и т.п.
- Generatestring (p, s);
- printf ("%s", s);
- // очистить память
+  int i, p, k;
+  char* s;
+  scanf("%d", &p);
+  k = (1 << p) - 1;
+  s = (char*)malloc(sizeof(char) * k);
+  Generatestring (p, s);
+  printf ("%s", s);
+  free (s);
+  return 0;
 }
 void Generatestring (int n, char* str)
 {
- int m=0 ,k;
- for (k = 1; k <= n; k++)
- {
-  strncpy(&str[m+1], str, sizeof(char) * m);
-  str[m] = 'a' + k - 1;
-  m = (1 << k) - 1;
- }
+  int m = 0, k;
+  for (k = 1; k <= n; k++)
+  {
+    strncpy(&str[m + 1], str, sizeof(char) * m);
+    str[m] = 'a' + k - 1;
+    m = (1 << k) - 1;
+  }
 }
  
