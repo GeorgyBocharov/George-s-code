ebea744a5315bea1a50df2b73d7aa26fdb4c633d
George <georgybocharovfrtc@gmail.com>
2017-11-17 22:17:55 +0300

    fixed version

--- straight_tree.c
+++ straight_tree.c
@@ -8,12 +8,9 @@ int main()
 {	
 	int  i = 0, n, *id;
 	pid_t father, pid;
-	id = (int*) calloc (n, sizeof (int));
-	/*
-	fixit: не кажется странным, что вы выделили память до того, как узнали количество процессов?
-	*/
 	printf ("enter amount of processes  ");
 	scanf ("%d", &n);
+	id = (int*) calloc (n, sizeof (int));
 	father = getpid();
 	printf ("First process: %d\n", father);
 	while (i != n)
@@ -35,17 +32,8 @@ int main()
 		{
 			waitpid (pid, &i, 0);
 			printf ("I am %d,  my child %d died\n", getpid (),  pid);
-			break;
-			/*
-			если я правильно понял, то здесь вместо break вы как раз можете написать exit.
-			и тогда цикл ниже можно будет удалить
-			*/
+			exit(-1);
 		}
-	}
-	for (i = 0;i < n; i++)
-	{
-		if (id[i] == getpid ())
-			exit (i);
 	}
 	return 0;
 }
